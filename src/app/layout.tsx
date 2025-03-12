efd562532f19c51fed7ceaff83d540b6eb612016
Łukasz Tryczynski <67525928+ltryczynski@users.noreply.github.com>
2025-03-12 22:57:10 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ import Container from "@/components/container";
 export const metadata: Metadata = {
   title: "Evento | Find events around you",
   description: "Browse more than 10,000 events worldwide",
+  robots: {
+    follow: false,
+    index: false,
+  }
 };
 
 export default function RootLayout({
